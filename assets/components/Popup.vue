8d6d60c8c220b340f74c333ca532abe302470f3e
Alan Leverenz <awleverenz@aol.com>
2021-03-31 00:55:51 -0400

    added vue and icons, updated webpack.mix

---
+++ assets/components/Popup.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="wrapper">
+        <h1 class="title">Simple Toggle Sites</h1>
+        <div class="buttons">
+            <button type="button" class="state-off" @click="setActive(false)" :class="{'is-active': !active}">Off</button>
+            <button type="button" class="state-on" @click="setActive(true)" :class="{'is-active': active}">On</button>
+        </div>
+        <div class="sites">
+            <p>List your websites below, one per line</p>
+            <textarea v-model="list" rows="8" autocomplete="off" autocorrect="off" autocapitalize="off" spellcheck="false"></textarea>
+            <button type="button" class="state-save" @click="saveList">Save Site List</button>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            active: true,
+            list: "example.com"
+        }
+    },
+    created() {
+        // to generate global variables
+        // (results) is the callback function
+        // can use chrome API even if we are using Vue
+        chrome.storage.sync.get(['toggleSitesActive', 'toggleSitesList'], (results) => {
+            this.active = result.toggleSitesActive;
+            this.list = result.toggleSitesList;
+        })
+    },
+    methods: {
+        // both functions null out any callback function
+        setActive(active) {
+            // sets active boolean value in chrome.storage
+            this.active = active;
+            chrome.storage.sync.set({
+                toggleSitesActive: active
+            }, () => {}); // null callback
+        },
+        saveList() {
+            chrome.storage.sync.set({
+                toggleSitesList: this.list
+            }, () => {});
+        }
+    }
+}
+</script>
